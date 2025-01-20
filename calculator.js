0643f1ededcfbfc983d17bc7aba0345dde02d91a
vboxuser <ben527466@gmail.com>
2025-01-20 15:25:29 +0700

    complete functions in calculator project

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@ function add(num1,num2){
 }
 function subtract(num1,num2){
     
-    return num1>num2?(num1-num2):-(num1-num2);
+    return num1-num2;
 }
 function multiply(num1,num2){
     return num1*num2;
@@ -11,43 +11,83 @@ function multiply(num1,num2){
 function divide(num1,num2){
     return num1/num2;
 }
-let num1 = null, num2 = null, operator = null;
+let num1 = ``, num2 = ``, operator =null;
+let result;
 
 function operate(num1,num2,operator){
 if(operator==="+"){
-    console.log(add(num1,num2))
+    return add(num1,num2)
 }else if(operator==="-"){
-    console.log(subtract(num1,num2))
+    return subtract(num1,num2)
 }else if(operator==="*"){
-    console.log(multiply(num1,num2))
+    return multiply(num1,num2)
 }else if(operator==="/"){
-    console.log(divide(num1,num2))
+    return divide(num1,num2)
 // }else if(operator==="="){
 
 // }
 }
 
 }
+
 function isNumber(value) {
     return !isNaN(parseFloat(value)) && isFinite(value);
 }
+const calculatorScreen=document.getElementById("screen");
+const numberInput=document.getElementById("calculation")
+
+
 const clickButton=document.querySelectorAll(".butt");
 clickButton.forEach(stuff=>{
     stuff.addEventListener("click",function(){
         if(stuff.textContent==="+"||stuff.textContent==="-"||stuff.textContent==="*"||stuff.textContent==="/"){
+
+            if(num1!=``&&num2!=``){
+                num1=operate(Number(num1),Number(num2),operator);
+                console.log(num1)
+                operator=null;
+                num2=``;
+                numberInput.textContent=`${num1}`;
+                
+            }
             operator=stuff.textContent;
-            console.log(operator)
-        }else if(isNumber(stuff.textContent)&&num1==null){
-            num1=Number(stuff.textContent);
+            numberInput.textContent+=`${operator}`;
+            calculatorScreen.appendChild(numberInput);
+        }else if(isNumber(stuff.textContent)&&operator==null){
+            num1+=Number(stuff.textContent);
+            numberInput.textContent=`${num1}`;
+           
             console.log(num1);
-        }else if(isNumber(stuff.textContent)&&num1!=null){
-            num2=Number(stuff.textContent);
+        }else if(isNumber(stuff.textContent)&&operator!=null){
+            num2+=Number(stuff.textContent);
+            numberInput.textContent=`${num2}`;
+         
             console.log(num2);
+           
         }else if(stuff.textContent=="="){
-            console.log(operate(num1,num2,operator));
-            num1=null;num2=null;operator=null;
-        }else if(stuff.textContent=="clear"){
-            num1=null;num2=null;operator=null;
+           
+            // num1=operate(Number(num1),Number(num2),operator)
+            // console.log(num1);
+            if(num1!=``&&num2!=``){
+                num1=operate(Number(num1),Number(num2),operator);
+                if(!isFinite(num1)){
+                    console.log("ERROR")
+                    numberInput.textContent=`ERROR`;
+                  
+                }else{
+                    operator=null;
+                    num2=``;
+                    console.log(num1)
+                    numberInput.textContent=`${num1}`;
+                
+                }
+               
+            }
+            
+        }else if(stuff.textContent=="AC"){
+            numberInput.textContent=`0`;
+            num1=``;num2=``;operator=null;
+            
         }
     })
 })
